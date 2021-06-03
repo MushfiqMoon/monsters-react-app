5ac36ecacc224ebbafc5aa9cd3ce9f6d0a5a6f66
mushfiq <mushfiqur.rahman@sjinnovation.com>
2021-06-04 05:05:25 +0600

    SPA lecture 29

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor() {
+    super();
+  
+    this.state = {
+      monster : [
+        {
+          name : 'Franky',
+          id : 'asd1'
+        },
+        {
+          name : 'Drako',
+          id : 'asd2'
+        },
+        {
+          name : 'Zomb',
+          id : 'asd3'
+        }
+        
+      ]
+      
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+      {
+        this.state.monster.map(monster => <h1 id = { monster.id }> {monster.name} </h1>)
+      }     
+      </div>
+    )
+  }
 }
 
 export default App;
